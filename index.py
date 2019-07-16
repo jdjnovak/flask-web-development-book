b213ca5396a117dc241d46eb5ddf637ce2d29857
Joshua Novak <jdjnovak@gmail.com>
2019-07-16 16:09:22 -0600

    Chapter 2 progress - requests and responses

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +11,7 @@ def index():
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
 
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
