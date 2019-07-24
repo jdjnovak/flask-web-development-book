6b1615983b7b5d0bfb0d4fe1b036bc709bcc724b
Joshua Novak <jdjnovak@gmail.com>
2019-07-24 14:05:00 -0600

    Ch 4 - Added a basic form on the index

--- index.py
+++ index.py
@@ -2,15 +2,28 @@ from flask import Flask, render_template, request, make_response, redirect
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+################### Secret Key #########################
+app.config['SECRET_KEY'] = 'oHsOsEcReTkEy6969'
+
+
+################### App Routes #########################
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
@@ -29,6 +42,7 @@ def temp_inher():
     return render_template('example_template.html')
 
 
+################# App Error Pages ####################
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -37,3 +51,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+################# Class Definitions ###################
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
